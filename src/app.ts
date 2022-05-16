4af184005bfa032b6b624c1c28e32a8b167c6976
Alexandra <alexandra_mitu98@yahoo.com>
2022-05-16 17:05:37 +0100

    Lesson 14 completed.

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,6 @@ import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
-// invOne.client = 'yoshi';
-// invOne.amount = 50;
-
 let invoices: Invoice[] = [];
 invoices.push(invOne)
 invoices.push(invTwo);
