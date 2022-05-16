dc4003e28788cdaef6338d6d34886c812a4efc4b
Alexandra <alexandra_mitu98@yahoo.com>
2022-05-16 10:28:48 +0100

    Lesson 6 completed.

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,20 @@
-var age = 25;
+var age; //Any is a dynamic type
+var year = 2022; //let year be of any type, but for now let's initialise it as 2022.
+//In Java, this would be something like "int year = 2022;" since there is no dynamic type in Java.
 age = true;
 console.log(age);
-age = 'hello';
+age = 'Old';
 console.log(age);
-age = { name: 'luigi' };
+age = {
+    old: true,
+    name: 'luigi'
+};
 console.log(age);
 var mixed = [];
 mixed.push(5);
 mixed.push('mario');
-mixed.push(false);
+mixed.push(true);
 console.log(mixed);
 var ninja;
 ninja = { name: 'yoshi', age: 25 };
-console.log(ninja);
-ninja = { name: 25, age: 'yoshi' };
 console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,24 +1,26 @@
-let age: any = 25;
+let age: any; //Any is a dynamic type
+let year: any = 2022; //let year be of any type, but for now let's initialise it as 2022.
+//In Java, this would be something like "int year = 2022;" since there is no dynamic type in Java.
 
 age = true;
 console.log(age);
-age = 'hello';
+
+age = 'Old';
 console.log(age);
-age = { name: 'luigi' };
+
+age = {
+  old: true,
+  name: 'luigi'
+};
 console.log(age);
 
 let mixed: any[] = [];
 
 mixed.push(5);
 mixed.push('mario');
-mixed.push(false);
+mixed.push(true);
 console.log(mixed);
 
-let ninja: { name: any, age: any };
-
-ninja = { name: 'yoshi', age: 25 };
+let ninja: { name: any, age: any};
+ninja = {name: 'yoshi', age: 25};
 console.log(ninja);
-
-ninja = { name: 25, age: 'yoshi' };
-console.log(ninja);
-
