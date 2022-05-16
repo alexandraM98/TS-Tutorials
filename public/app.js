c647b3d8dcbb91918f74610f4c231a54bb6d4999
Alexandra <alexandra_mitu98@yahoo.com>
2022-05-16 15:32:39 +0100

    Lesson 11 completed.

--- public/app.js
+++ public/app.js
@@ -1,18 +1,14 @@
 "use strict";
-var anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
-}
-console.log(anchor.href);
+// const anchor = document.querySelector('a')!; //!; use this if you definitely know that element is not null.
+// // if (anchor) {
+// //   console.log(anchor.href);
+// // }
+// console.log(anchor.href);
 //const form = document.querySelector('form')!;
 var form = document.querySelector('.new-item-form');
 console.log(form.children);
-// inputs
 var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+form.addEventListener(('submit'), function (e) {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    console.log(type.value);
 });
